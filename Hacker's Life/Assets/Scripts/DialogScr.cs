c27e0e573c6c527ccc55212c62316336cc018a51
Kayyy GameDeveloper <48172223+KayDevv@users.noreply.github.com>
2019-04-28 12:05:50 +0300

    v0.0
    Update:
    - Dekstop + Panel
    - DialogScr

---
+++ Hacker's Life/Assets/Scripts/DialogScr.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogPerson
+{
+    public Sprite Face;
+    public string Name;
+
+    public DialogPerson(string name, string facePath)
+    {
+        Name = name;
+        Face = Resources.Load<Sprite>(facePath);
+    }
+}
+
+public class DialogPhrase
+{
+    public DialogPerson Speaker;
+    public string Message;
+
+    public DialogPhrase(DialogPerson person, string msg)
+    {
+        Speaker = person;
+        Message = msg;
+    }
+}
+
+public class Dialog
+{
+    public enum DialogType
+    {
+        FIRST_DIALOG
+    }
+
+    public DialogType Type;
+    public List<DialogPhrase> Phrases = new List<DialogPhrase>();
+
+    public Dialog(DialogType type)
+    {
+        Type = type;
+    }
+
+    public void AddPhrase(DialogPhrase phrase)
+    {
+        Phrases.Add(phrase);
+    }
+}
+
+public static class DialogManager
+{
+    public static List<Dialog> Dialogs = new List<Dialog>();
+
+    public static void AddDialog(Dialog dialog)
+    {
+        Dialogs.Add(dialog);
+    }
+}
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void Start()
+    {
+        DialogPerson hack3r = new DialogPerson("HACKER", "hack3e");
+        DialogPerson user = new DialogPerson("USER", "user");
+
+        Dialog firstDialog = new Dialog(Dialog.DialogType.FIRST_DIALOG);
+
+        firstDialog.AddPhrase(new DialogPhrase(user, "Привет! Взломай страницу моего босса на http://vk.ru! Логин: b0ss"));
+        firstDialog.AddPhrase(new DialogPhrase(gnojniy, "Цена?"));
+        //secondDialog.AddPhrase(new DialogPhrase(user, "10.000 рублей"));
+
+        DialogManager.AddDialog(firstDialog);
+    }
+}
+
+
---
+++ Hacker's Life/Assets/Scripts/PlayDialogScr.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using DG.Tweening;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Dialog.DialogType DialogType;
+    public DIalogObjScr Dialog;
+
+    private void Awake()
+    {
+        //DOTween.Init();
+    }
+
+    
+
+}
